d06b32c4a564a9ad51c1b788ab47316e283ee899
vincent <v.guevorte@laposte.net>
2020-05-20 13:44:38 +0200

    exo4

--- index.php
+++ index.php
@@ -35,26 +35,23 @@
 
 // echo $n.' '.$m;
 
-// $array1 = ["fruits" => ['pomme', 'poire', 'banane']];
+
+//   $array1 = ["fruits" => ['pomme', 'poire', 'banane']];
 //   $array2 = ["vehicules" => ['voiture', 'motos', 'avion']];
 //   $array3 = ["pays" => ['italie', 'france', 'belgique']];
 
-  $array1 = ["fruits" => ['pomme', 'poire', 'banane']];
-  $array2 = ["vehicules" => ['voiture', 'motos', 'avion']];
-  $array3 = ["pays" => ['italie', 'france', 'belgique']];
+// $arrayGlobal = array_merge($array1, $array2, $array3);
 
-$arrayGlobal = array_merge($array1, $array2, $array3);
+//   var_dump($arrayGlobal);
 
-  var_dump($arrayGlobal);
+//   echo '<ul>';
+//   foreach($arrayGlobal as $key => $array)
+//   {
 
-  echo '<ul>';
-  foreach($arrayGlobal as $key => $array)
-  {
-
-      echo '<li>'.$key . '<ul>';
-      foreach($array as $value)
-      {
-          echo '<li>'.$value . '</li>';
-      }
-      echo '</ul></li>';
-  }
+//       echo '<li>'.$key . '<ul>';
+//       foreach($array as $value)
+//       {
+//           echo '<li>'.$value . '</li>';
+//       }
+//       echo '</ul></li>';
+//   }
